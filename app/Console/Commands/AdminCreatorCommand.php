51e88de05703fc2f14fb71a692946627d4eeae48
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2017-03-28 16:10:23 +0300

    commit 28/3

--- app/Console/Commands/AdminCreatorCommand.php
+++ app/Console/Commands/AdminCreatorCommand.php
@@ -47,7 +47,7 @@ class AdminCreatorCommand extends Command
           $answer = $this->ask($question);
           array_push($answers,$answer);
         }
-        f
+        
         User::create(['name' => 'Admin', 'email' => $answers[0], 'password' => bcrypt($answers[1]), 'function' => 'Admin', 'department' => 'Admin', 'telephone' => $answers[2], 'address' => $answers[3], 'admin' => 1]);
 
         $this->line('Admin Created!');
